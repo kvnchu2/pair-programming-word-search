16e739a007329664b6d1d7598d10c9026f5a31c2
Kevin Chu <kvnchu2@gmail.com>
2020-12-03 02:10:29 +0000

    completed pair programming exercise

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,48 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Created by Jacqueline Smith, Kevin Chu, and Aqil Ebrahim 
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => { 
+    const wordArray = []
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    wordArray.push(horizontalJoin);
+    const verticalJoin = function(letters) {
+      
+        for (row = 0; row < letters[0].length; row++) {
+          let string = '';
+          for (column = 0; column < letters.length; column++) {
+            string += letters[column][row];
+          }
+          wordArray.push(string);
+        }
+    }
+    wordArray.push(verticalJoin(letters));
+    
+    console.log(wordArray)
+    
+    for (l of wordArray) {
+    // if the word is present (horizontally) return true
+        if (l.includes(word)) {
+             return true
+            } else {
+             return false;
+    };
+    
+    }
+}    
+
+console.log(wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ]));
+
+
+
+
+
+module.exports = wordSearch;
